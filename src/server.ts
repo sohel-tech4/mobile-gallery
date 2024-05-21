99fb70662ecdd548d338e645ab00b3cd1df2f9fa
Sohel Rana <soheltech4@gmail.com>
2024-05-21 09:06:45 +0600

    run server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
-const mongoose = require("mongoose");
-
-main().catch((err) => console.log(err));
+import app from "./app";
+import mongoose from "mongoose";
+import config from "./config";
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  try {
+    await mongoose.connect(config.db_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+main();
